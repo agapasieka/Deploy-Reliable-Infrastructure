e034ec5328c06fb58cd074feda88cda9be1d59d7
Agnieszka Pasieka <61354571+agapasieka@users.noreply.github.com>
2024-09-28 12:32:52 +0100

    Update bucket.tf

--- Part2-Deploy-App-to-VM/bucket.tf
+++ Part2-Deploy-App-to-VM/bucket.tf
@@ -1,6 +1,6 @@
 # Create a GCS bucket
 resource "google_storage_bucket" "public_bucket" {
-  name         = var.project_id  
+  name         = "${var.project_id}-bucket"
   location      = "EU"
   force_destroy = true
 
@@ -12,15 +12,15 @@ resource "google_storage_bucket" "public_bucket" {
 
 # Upload two files to the bucket with public access
 resource "google_storage_bucket_object" "file1" {
-  name   = "blog.html"   # Name of the file in the bucket
+  name   = "blog.html"   
   bucket = google_storage_bucket.public_bucket.name
-  source = "blog.html"   # Path to the local file
+  source = "scripts/blog.html"   
   content_type = "text/html"
 }
 
 resource "google_storage_bucket_object" "file2" {
-  name   = "my-dog.jpg"  # Name of the file in the bucket
+  name   = "my-dog.jpg"  
   bucket = google_storage_bucket.public_bucket.name
-  source = "my-dog.jpg"  # Path to the local file
+  source = "scripts/my-dog.jpg"  
   content_type = "image/jpeg"
 }
