69f7af4d061bf584305adc15dcb72ab9953674de
Agnieszka Pasieka <61354571+agapasieka@users.noreply.github.com>
2024-09-28 12:48:52 +0100

    Update iam.tf

--- Part2-Deploy-App-to-VM/iam.tf
+++ Part2-Deploy-App-to-VM/iam.tf
@@ -1,6 +1,6 @@
-# Grant the service account permission to read objects in the bucket
-resource "google_storage_bucket_iam_member" "vm_access" {
-  bucket = google_storage_bucket.public_bucket.name
+# Grant the service account permission to read objects in the buckets
+resource "google_project_iam_member" "vm_access" {
+  project = var.project_id
   role   = "roles/storage.objectViewer"
 
   member = "serviceAccount:${google_service_account.blog_vm_sa.email}"
