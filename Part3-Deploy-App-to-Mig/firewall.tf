9de9094d6ef6912cd5985622d4dbf5ed7209da12
Agnieszka Pasieka <61354571+agapasieka@users.noreply.github.com>
2024-10-03 12:05:00 +0100

    Update firewall.tf

--- Part3-Deploy-App-to-Mig/firewall.tf
+++ Part3-Deploy-App-to-Mig/firewall.tf
@@ -1,18 +1,4 @@
-# Allow SSH
-resource "google_compute_firewall" "allow_ssh" {
-  name = "${local.name}-fwrule-allow-ssh22"
-  allow {
-    ports    = ["22"]
-    protocol = "tcp"
-  }
-  direction     = "INGRESS"
-  network       = google_compute_network.myvpc.id
-  priority      = 1000
-  source_ranges = ["0.0.0.0/0"]
-  target_tags   = ["ssh-tag"]
-}
-
-# Allow HTTP Port 80
+# Allow HTTP Port 80 from Proxy-only subnet
 resource "google_compute_firewall" "allow_http" {
   name = "${local.name}-fwrule-allow-http80"
   allow {
@@ -22,7 +8,7 @@ resource "google_compute_firewall" "allow_http" {
   direction     = "INGRESS"
   network       = google_compute_network.myvpc.id
   priority      = 1000
-  source_ranges = ["0.0.0.0/0"]
+  source_ranges = [""]
   target_tags   = ["webserver-tag"]
 }
 
