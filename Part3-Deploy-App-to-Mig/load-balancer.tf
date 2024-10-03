c7fd020d97ef7dd436a85a6b4c609c872488e6a3
Agnieszka Pasieka <61354571+agapasieka@users.noreply.github.com>
2024-10-03 15:22:13 +0100

    Update load-balancer.tf

--- Part3-Deploy-App-to-Mig/load-balancer.tf
+++ Part3-Deploy-App-to-Mig/load-balancer.tf
@@ -1,11 +1,11 @@
-# Reserve Regional Static IP Address
-resource "google_compute_address" "mylb" {
-  name   = "${local.name}-mylb-regional-static-ip"
+# Reserve Static IP Address
+resource "google_compute_global_address" "mylb" {
+  name   = "${local.name}-mylb-static-ip"
   region = var.region
 }
 
-# Regional Health Check
-resource "google_compute_region_health_check" "mylb" {
+# Health Check
+resource "google_compute_health_check" "mylb" {
   name                = "${local.name}-mylb-blog-health-check"
   check_interval_sec  = 5
   timeout_sec         = 5
@@ -18,40 +18,40 @@ resource "google_compute_region_health_check" "mylb" {
 }
 
 # Regional Backend Service
-resource "google_compute_region_backend_service" "mylb" {
+resource "google_compute_backend_service" "mylb" {
   name                  = "${local.name}-blog-backend-service"
   protocol              = "HTTP"
   load_balancing_scheme = "EXTERNAL_MANAGED"
-  health_checks         = [google_compute_region_health_check.mylb.self_link]
+  health_checks         = [google_compute_health_check.mylb.self_link]
   port_name             = "webserver"
   backend {
-    group           = google_compute_region_instance_group_manager.blog_mig.instance_group
+    group           = google_compute_instance_group_manager.blog_mig.instance_group
     capacity_scaler = 1.0
     balancing_mode  = "UTILIZATION"
   }
 }
 
 # Regional URL Map
-resource "google_compute_region_url_map" "mylb" {
+resource "google_compute_url_map" "mylb" {
   name            = "${local.name}-mylb-url-map"
-  default_service = google_compute_region_backend_service.mylb.self_link
+  default_service = google_compute_backend_service.mylb.self_link
 }
 
 
 # Regional HTTP Proxy
-resource "google_compute_region_target_http_proxy" "mylb" {
+resource "google_compute_target_http_proxy" "mylb" {
   name    = "${local.name}-mylb-http-proxy"
-  url_map = google_compute_region_url_map.mylb.self_link
+  url_map = google_compute_url_map.mylb.self_link
 }
 
 
 # Regional Forwarding Rule
 resource "google_compute_forwarding_rule" "mylb" {
   name                  = "${local.name}-mylb-forwarding-rule"
-  target                = google_compute_region_target_http_proxy.mylb.self_link
+  target                = google_compute_target_http_proxy.mylb.self_link
   port_range            = "80"
   ip_protocol           = "TCP"
-  ip_address            = google_compute_address.mylb.address
+  ip_address            = google_compute_global_address.mylb.address
   load_balancing_scheme = "EXTERNAL_MANAGED"
   network               = google_compute_network.myvpc.id
 
@@ -59,7 +59,7 @@ resource "google_compute_forwarding_rule" "mylb" {
 }
 
 output "Load_Balancer_IP" {
-  value       = google_compute_address.mylb.address
+  value       = google_compute_global_address.mylb.address
   description = "The external IP address of the Load Balancer"
 }
 
