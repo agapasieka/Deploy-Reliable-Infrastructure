b904f1c1b9c78174cb861cdbb24d0d911bf3d333
Agnieszka Pasieka <61354571+agapasieka@users.noreply.github.com>
2024-10-03 15:55:44 +0100

    Delete Part3-Deploy-App-to-Mig/iam.tf

--- Part3-Deploy-App-to-Mig/iam.tf
+++
@@ -1,9 +0,0 @@
-# Add bucket-level access control to allow public access
-resource "google_storage_bucket_iam_binding" "bucket_public_access" {
-  bucket = google_storage_bucket.public_bucket.name
-  role   = "roles/storage.objectViewer"
-
-  members = [
-    "allUsers",
-  ]
-}
