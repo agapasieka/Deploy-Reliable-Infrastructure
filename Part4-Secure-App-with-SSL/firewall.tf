8948a69f36c295b8811b56e46d71211e55e64e60
Agnieszka Pasieka <61354571+agapasieka@users.noreply.github.com>
2024-10-05 10:48:52 +0100

    Update firewall.tf

--- Part4-Secure-App-with-SSL/firewall.tf
+++ Part4-Secure-App-with-SSL/firewall.tf
@@ -1,18 +1,4 @@
-# Allow SSH
-resource "google_compute_firewall" "allow_ssh" {
-  name = "${local.name}-fwrule-allow-ssh22"
-  allow {
-    ports    = ["22"]
-    protocol = "tcp"
-  }
-  direction     = "INGRESS"
-  network       = google_compute_network.myvpc.id
-  priority      = 1000
-  source_ranges = ["0.0.0.0/0"]
-  target_tags   = ["ssh-tag"]
-}
-
-# Allow HTTP Port 80
+# Allow HTTP from Proxy-only subnet to backends
 resource "google_compute_firewall" "allow_http" {
   name = "${local.name}-fwrule-allow-http80"
   allow {
@@ -22,7 +8,7 @@ resource "google_compute_firewall" "allow_http" {
   direction     = "INGRESS"
   network       = google_compute_network.myvpc.id
   priority      = 1000
-  source_ranges = ["0.0.0.0/0"]
+  source_ranges = ["10.0.0.0/24"]
   target_tags   = ["webserver-tag"]
 }
 
