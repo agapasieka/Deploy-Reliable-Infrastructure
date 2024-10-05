ae186882816c8dd3ca145b210e9314affec2d03c
Agnieszka Pasieka <61354571+agapasieka@users.noreply.github.com>
2024-10-05 10:56:30 +0100

    Update bucket.tf

--- Part4-Secure-App-with-SSL/bucket.tf
+++ Part4-Secure-App-with-SSL/bucket.tf
@@ -24,3 +24,13 @@ resource "google_storage_bucket_object" "file2" {
   source       = "scripts/my-dog.jpg"
   content_type = "image/jpeg"
 }
+
+# Add bucket-level access control to allow public access
+resource "google_storage_bucket_iam_binding" "bucket_public_access" {
+  bucket = google_storage_bucket.public_bucket.name
+  role   = "roles/storage.objectViewer"
+
+  members = [
+    "allUsers",
+  ]
+}
